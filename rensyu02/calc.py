5b83460efbe06fb38e724357571640385f4f65a7
fushimity <fushimity@edu.teu.ac.jp>
2022-04-18 18:32:44 +0900

    add click event

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -1,6 +1,12 @@
 import tkinter as tk
+import tkinter.messagebox as tkmb
 
-def main():
+def click_button(event):
+    btn = event.widget
+    tkmb.showinfo("", f"{btn['text']}のボタンがクリックされました")
+
+
+if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("300x450")
     frm = tk.Frame(root)
@@ -8,6 +14,7 @@ def main():
     buttons = []
     for i in range(9, -1, -1):
         btn = tk.Button(root, text=f"{i}", font=("Times New Roman", 30))
+        btn.bind("<1>", click_button)
         buttons.append(btn)
     r, c = 0, 0
     for i, btn in enumerate(buttons):
@@ -17,5 +24,3 @@ def main():
         c += 1
     root.mainloop()
 
-if __name__ == "__main__":
-    main()
