c8062ac160bc0ebe139a3e3f0eae06c81dec9c72
fushimity <fushimity@edu.teu.ac.jp>
2022-04-18 19:12:23 +0900

    merge for loops

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -13,13 +13,10 @@ if __name__ == "__main__":
     entry = tk.Entry(root, justify="right", width="10", font=("Times New Roman", 40))
     entry.grid(row=0, column=0, padx=10, pady=10, columnspan=3)
 
-    buttons = []
-    for i in range(9, -1, -1):
-        btn = tk.Button(root, text=f"{i}", font=("Times New Roman", 30))
-        btn.bind("<1>", click_button)
-        buttons.append(btn)
     r, c = 1, 0
-    for i, btn in enumerate(buttons):
+    for i, num in enumerate(range(9, -1, -1)):
+        btn = tk.Button(root, text=f"{num}", font=("Times New Roman", 30))
+        btn.bind("<1>", click_button)
         if i%3 == 0:
             r, c = r+1, 0
         btn.grid(row=r, column=c, padx=10, pady=10)
