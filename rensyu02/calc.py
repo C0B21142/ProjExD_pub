0fe1bd59a737cac6fc29c7d1fc936edda8b2b21d
fushimity <fushimity@edu.teu.ac.jp>
2022-04-18 17:01:07 +0900

    align buttons

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -3,12 +3,18 @@ import tkinter as tk
 def main():
     root = tk.Tk()
     root.geometry("300x450")
+    frm = tk.Frame(root)
+    
     buttons = []
-    for i in range(10):
+    for i in range(9, -1, -1):
         btn = tk.Button(root, text=f"{i}", font=("Times New Roman", 30))
         buttons.append(btn)
-    for btn in buttons:
-        btn.pack()
+    r, c = 0, 0
+    for i, btn in enumerate(buttons):
+        if i%3 == 0:
+            r, c = r+1, 0
+        btn.grid(row=r, column=c)
+        c += 1
     root.mainloop()
 
 if __name__ == "__main__":
