e194201a4dcb22c9a954a3e79548c463eb0dcc3f
fushimity <fushimity@edu.teu.ac.jp>
2022-04-18 19:27:20 +0900

    add plus and equal buttons

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -6,6 +6,12 @@ def click_button(event):
     # tkmb.showinfo("", f"{btn['text']}のボタンがクリックされました")
     entry.insert(tk.END, btn["text"])
 
+def click_equal(event):
+    btn = event.widget
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, res)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -14,11 +20,15 @@ if __name__ == "__main__":
     entry.grid(row=0, column=0, padx=10, pady=10, columnspan=3)
 
     r, c = 1, 0
-    for i, num in enumerate(range(9, -1, -1)):
+    for i, num in enumerate(["7","8","9","4","5","6","1","2","3","0","+"]):
         btn = tk.Button(root, text=f"{num}", font=("Times New Roman", 30))
         btn.bind("<1>", click_button)
         if i%3 == 0:
             r, c = r+1, 0
         btn.grid(row=r, column=c, padx=10, pady=10)
         c += 1
+    btn = tk.Button(root, text="=", font=("Times New Roman", 30))
+    btn.bind("<1>", click_equal)
+    btn.grid(row=5, column=2, padx=10, pady=10)
+
     root.mainloop()
