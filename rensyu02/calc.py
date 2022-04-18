eec377bd7b5ccf7aaa4372304c243d538342f881
fushimity <fushimity@edu.teu.ac.jp>
2022-04-18 16:27:11 +0900

    add buttons

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -3,6 +3,12 @@ import tkinter as tk
 def main():
     root = tk.Tk()
     root.geometry("300x450")
+    buttons = []
+    for i in range(10):
+        btn = tk.Button(root, text=f"{i}")
+        buttons.append(btn)
+    for btn in buttons:
+        btn.pack()
     root.mainloop()
 
 if __name__ == "__main__":
