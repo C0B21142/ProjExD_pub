03d436bd4b899276c37e754ddc7094a4fce6736a
fushimity <fushimity@edu.teu.ac.jp>
2022-04-26 14:36:08 +0900

    数字ボタンのフォントを設定

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     r, c = 0, 1
     for i, num in enumerate(range(9, -1, -1), 1):
-        btn = tk.Button(root, text=num)
+        btn = tk.Button(root, text=num, font=("Times New Roman", 30))
         btn.grid(row=r, column=c)
         if i%3 == 0:
             r += 1
