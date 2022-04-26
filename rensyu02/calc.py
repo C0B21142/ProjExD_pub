cf3789d8fd0a89e08342cc6cf32ef1e777ce9499
fushimity <fushimity@edu.teu.ac.jp>
2022-04-26 14:56:12 +0900

    クリックされたボタンの数字を表示する

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -1,4 +1,10 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]  # クリックされたボタンの数字
+    tkm.showinfo(num, f"{num}のボタンがクリックされました")
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -7,6 +13,7 @@ if __name__ == "__main__":
     r, c = 0, 1
     for i, num in enumerate(range(9, -1, -1), 1):
         btn = tk.Button(root, text=num, font=("Times New Roman", 30))
+        btn.bind("<1>", button_click)
         btn.grid(row=r, column=c, padx=10, pady=10)
         if i%3 == 0:
             r += 1
