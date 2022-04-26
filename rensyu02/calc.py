da785acf906d7aa946ef372d222aa1f7864ffe22
fushimity <fushimity@edu.teu.ac.jp>
2022-04-26 14:34:42 +0900

    数字ボタン作成

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -3,4 +3,14 @@ import tkinter as tk
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("300x450")
+
+    r, c = 0, 1
+    for i, num in enumerate(range(9, -1, -1), 1):
+        btn = tk.Button(root, text=num)
+        btn.grid(row=r, column=c)
+        if i%3 == 0:
+            r += 1
+            c = 0
+        c += 1
+        
     root.mainloop()
